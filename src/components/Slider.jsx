ef1e56982336f71bd10c7a08200afe90d1b7ba9f
rashiq <mail2.rashiq@gmail.com>
2024-09-09 20:04:30 +0530

    log console values

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,32 @@
+import { useRef } from "react";
 export default function Slider({ sliderName }) {
+  const sliderRef = useRef();
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Process the form data here
+    console.log("Name:", sliderRef.current.value);
+  }
   return (
     <>
-      <div>
-        <label htmlFor={sliderName}>
-          {sliderName}
-          <input type="range" min={1} max={100} id={sliderName} />
-        </label>
-        <p>Additional Message</p>
-        <textarea name="" id=""></textarea>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor={sliderName}>
+            {sliderName}
+            <input
+              type="range"
+              min={1}
+              max={100}
+              id={sliderName}
+              name={sliderName}
+              onChange={(e) => e.target.value}
+              ref={sliderRef}
+            />
+          </label>
+          <p>Additional Message</p>
+          <textarea name="" id=""></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
